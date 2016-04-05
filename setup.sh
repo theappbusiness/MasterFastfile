7688af9f1f490ac55fd84269c1556f32a03d3d67
Neil Horton <neil.horton@theappbusiness.com>
2016-04-05 17:15:55 +0100

    Add default empty default variables

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ echo "fastlane_version \"1.55.0\"
 import_from_git(url: 'git@bitbucket.org:theappbusiness/tabfastlanemaster-ios.git', path: 'Fastfile')" > Fastfile
 echo "#This is your default environment file
 #Set environment variables used in all builds here
-FL_HOCKEY_API_TOKEN=f99b82982b164abaa5963c5caa09ec1e
-FL_HOCKEY_OWNER_ID=467137
+FL_HOCKEY_API_TOKEN= #Hocky API Token
+FL_HOCKEY_OWNER_ID= #Hockey Organisation ID (number not name)
 FL_UPDATE_PLIST_PATH= #Path to Info.plist
-GYM_CODE_SIGNING_IDENTITY=\"iPhone Distribution: The App Business LTD\"
-FL_HOCKEY_TEAMS=\"23022\"
+GYM_CODE_SIGNING_IDENTITY= #Code Sign Identitify
+FL_HOCKEY_TEAMS= #Hockey ID (number not name)
 FL_HOCKEY_NOTIFY= #Email team when new build avialable? 0 = No, 1 = Yes
 ICON_OVERLAY_ASSETS_BUNDLE= #Path to .xcassets
 TAB_USE_TIME_FOR_BUILD_NUMBER= #Use Time and date for build number or BUILD_NUMBER environment variable (created by jenkis or team city) true = use time, false = use BUILD_NUMBER" > .env.default
