239f80faf1cc84f0a84bfd9c5def4d83b430a25c
Neil Horton <neil.horton@theappbusiness.com>
2016-04-05 11:09:16 +0100

    Improve default files created for TAB only

--- README.md
+++ README.md
@@ -7,7 +7,24 @@
 $ curl https://raw.githubusercontent.com/theappbusiness/MasterFastfile/master/setup.sh | sh
 ```
 
-Then setup your environments using `.env` files
+Then setup your environments using the `.env` files created for you. Several non project specific variables are already set, to complete the process just add the missing variable values.
+
+ In the `default.env` add values for the following parameters
+* FL_UPDATE_PLIST_PATH - Path to Info.plist
+* FL_HOCKEY_NOTIFY - Email team when new build available?
+  * 0 = No
+  * 1 = Yes
+* ICON_OVERLAY_ASSETS_BUNDLE - Path to .xcassets
+* TAB_USE_TIME_FOR_BUILD_NUMBER - Use Time and date for build number or BUILD_NUMBER environment variable (created by jenkis or team city)
+  * true = Use time and date
+  * false = Use BUILD_NUMBER
+
+In the `.env` file for each environment add values for the following parameters
+
+* GYM_SCHEME - Scheme to build for the current environment.
+* FL_UPDATE_PLIST_APP_IDENTIFIER - App Bundle Identifier
+* FL_UPDATE_PLIST_DISPLAY_NAME - Display name of app
+
 
 ## Available lanes
 
--- setup.sh
+++ setup.sh
@@ -2,16 +2,36 @@
 
 mkdir fastlane
 cd fastlane
-echo "app_identifier = ENV['APP_ID']" > Appfile
+echo "app_identifier = ENV['FL_UPDATE_PLIST_APP_IDENTIFIER']" > Appfile
 echo "fastlane_version \"1.55.0\"
 import_from_git(url: 'git@bitbucket.org:theappbusiness/tabfastlanemaster-ios.git', path: 'Fastfile')" > Fastfile
 echo "#This is your default environment file
-#Set environment variables used in all builds here" > .env.default
+#Set environment variables used in all builds here
+FL_HOCKEY_API_TOKEN=f99b82982b164abaa5963c5caa09ec1e
+FL_HOCKEY_OWNER_ID=467137
+FL_UPDATE_PLIST_PATH= #Path to Info.plist
+GYM_CODE_SIGNING_IDENTITY=\"iPhone Distribution: The App Business LTD\"
+FL_HOCKEY_TEAMS=\"23022\"
+FL_HOCKEY_NOTIFY= #Email team when new build avialable? 0 = No, 1 = Yes
+ICON_OVERLAY_ASSETS_BUNDLE= #Path to .xcassets
+TAB_USE_TIME_FOR_BUILD_NUMBER= #Use Time and date for build number or BUILD_NUMBER environment variable (created by jenkis or team city) true = use time, false = use BUILD_NUMBER" > .env.default
 echo "#This is your test environment file
-#Set environment variables used in test builds here" > .env.test
+#Set environment variables used in test builds here
+GYM_SCHEME= #Scheme name
+FL_UPDATE_PLIST_APP_IDENTIFIER= #App Bundle Identifier
+FL_UPDATE_PLIST_DISPLAY_NAME= #Display name of app" > .env.test
 echo "#This is your uat environment file
-#Set environment variables used in uat builds here" > .env.uat
+#Set environment variables used in uat builds here
+GYM_SCHEME= #Scheme name
+FL_UPDATE_PLIST_APP_IDENTIFIER= #App Bundle Identifier
+FL_UPDATE_PLIST_DISPLAY_NAME= #Display name of app" > .env.uat
 echo "#This is your stage environment file
-#Set environment variables used in stage builds here" > .env.stage
+#Set environment variables used in stage builds here
+GYM_SCHEME= #Scheme name
+FL_UPDATE_PLIST_APP_IDENTIFIER= #App Bundle Identifier
+FL_UPDATE_PLIST_DISPLAY_NAME= #Display name of app" > .env.stage
 echo "#This is your prod environment file
-#Set environment variables used in prod builds here" > .env.prod
+#Set environment variables used in prod builds here
+GYM_SCHEME= #Scheme name
+FL_UPDATE_PLIST_APP_IDENTIFIER= #App Bundle Identifier
+FL_UPDATE_PLIST_DISPLAY_NAME= #Display name of app" > .env.prod
