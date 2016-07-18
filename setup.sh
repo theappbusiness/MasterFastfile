58758a156cd999636bc4c365916018682113eec8
@dodsios <dods.sam@gmail.com>
2016-07-18 10:10:01 +0100

    refactored installation shell script to remove duplication

--- setup.sh
+++ setup.sh
@@ -5,7 +5,15 @@ cd fastlane
 echo "app_identifier = ENV['FL_UPDATE_PLIST_APP_IDENTIFIER']" > Appfile
 echo "fastlane_version \"1.55.0\"
 import_from_git(url: 'https://github.com/theappbusiness/MasterFastfile.git', path: 'Fastfile')" > Fastfile
-echo "#This is your default environment file
+make_default_env_file()
+make_custom_env_file("test")
+make_custom_env_file("uat")
+make_custom_env_file("staging")
+make_custom_env_file("prod")
+
+function make_default_env_file() {
+  cat > .env.default <<EOF
+#This is your default environment file
 #Set environment variables used in all builds here
 #More information on available environment variables can be found here https://github.com/theappbusiness/MasterFastfile/wiki/Quick-simple-setup-using-TAB-defaults
 FL_HOCKEY_API_TOKEN="" #Hocky API Token
@@ -15,24 +23,17 @@ GYM_CODE_SIGNING_IDENTITY="" #Code Sign Identitify
 FL_HOCKEY_TEAMS="" #Hockey ID (number not name)
 FL_HOCKEY_NOTIFY= #Email team when new build avialable? 0 = No, 1 = Yes
 ICON_OVERLAY_ASSETS_BUNDLE="" #Path to .xcassets
-TAB_USE_TIME_FOR_BUILD_NUMBER= #Use Time and date for build number or BUILD_NUMBER environment variable (created by jenkis or team city) true = use time, false = use BUILD_NUMBER" > .env.default
-echo "#This is your test environment file
-#Set environment variables used in test builds here
+TAB_USE_TIME_FOR_BUILD_NUMBER= #Use Time and date for build number or BUILD_NUMBER environment variable (created by jenkis or team city) true = use time, false = use BUILD_NUMBER
+EOF
+}
+
+function make_custom_env_file() {
+  env=$0
+  cat > .env.$env <<EOF
+#This is your ${env} environment file
+#Set environment variables used in ${env} builds here
 GYM_SCHEME="" #Scheme name
 FL_UPDATE_PLIST_APP_IDENTIFIER="" #App Bundle Identifier
-FL_UPDATE_PLIST_DISPLAY_NAME="" #Display name of app" > .env.test
-echo "#This is your uat environment file
-#Set environment variables used in uat builds here
-GYM_SCHEME="" #Scheme name
-FL_UPDATE_PLIST_APP_IDENTIFIER="" #App Bundle Identifier
-FL_UPDATE_PLIST_DISPLAY_NAME="" #Display name of app" > .env.uat
-echo "#This is your stage environment file
-#Set environment variables used in stage builds here
-GYM_SCHEME="" #Scheme name
-FL_UPDATE_PLIST_APP_IDENTIFIER="" #App Bundle Identifier
-FL_UPDATE_PLIST_DISPLAY_NAME="" #Display name of app" > .env.stage
-echo "#This is your prod environment file
-#Set environment variables used in prod builds here
-GYM_SCHEME="" #Scheme name
-FL_UPDATE_PLIST_APP_IDENTIFIER="" #App Bundle Identifier
-FL_UPDATE_PLIST_DISPLAY_NAME="" #Display name of app" > .env.prod
+FL_UPDATE_PLIST_DISPLAY_NAME="" #Display name of app
+EOF
+}
