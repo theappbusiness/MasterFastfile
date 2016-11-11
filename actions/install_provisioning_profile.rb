17f3efbb7fcc31beb607a54b44a3563746a49fe6
Luciano Marisi <luciano@theappbusiness.com>
2016-11-11 16:47:51 +0000

    Added message for failing action

--- actions/install_provisioning_profile.rb
+++ actions/install_provisioning_profile.rb
@@ -7,12 +7,15 @@ module Fastlane
     class InstallProvisioningProfileAction < Action
       def self.run(params)
         # fastlane will take care of reading in the parameter and fetching the environment variable:
-        # UI.message "Parameter API Token: #{params[:api_token]}"
+        # I.message "Parameter API Token: #{params[:api_token]}
         if ENV['TAB_PROVISIONING_PROFILE_PATH'] != nil
           provisioning_profile_path="../#{ENV['TAB_PROVISIONING_PROFILE_PATH']}" # needed because fastlane runs in the fastlane directory
           provisioning_profile_uuid = `grep UUID -A1 -a #{provisioning_profile_path} | grep -io \"[-A-Z0-9]\\{36\\}\"`
           provisioning_profile_destination = "#{ENV['HOME']}/Library/MobileDevice/Provisioning\\\ Profiles/#{provisioning_profile_uuid.strip}.mobileprovision"
           `cp #{provisioning_profile_path} #{provisioning_profile_destination}`
+          UI.message("Install profile succesfully")
+        else 
+          UI.message "Parameter API Token: #{params[:provisioning_profile_path]}"
         end
       end
 
@@ -26,7 +29,7 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :development,
+          FastlaneCore::ConfigItem.new(key: :provisioning_profile_path,
                                        env_name: "TAB_PROVISIONING_PROFILE_PATH",
                                        description: "The path of the provisioning profile to install",
                                        is_string: false, # true: verifies the input is a string, false: every kind of value
