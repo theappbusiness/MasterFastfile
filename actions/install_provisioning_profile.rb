2d1339d0ea6ab2dea8c64983435337ee6b224bca
Luciano Marisi <luciano@theappbusiness.com>
2016-11-11 16:59:24 +0000

    Removed debug logs

--- actions/install_provisioning_profile.rb
+++ actions/install_provisioning_profile.rb
@@ -6,20 +6,14 @@ module Fastlane
 
     class InstallProvisioningProfileAction < Action
       def self.run(params)
-        # fastlane will take care of reading in the parameter and fetching the environment variable:
-        # I.message "Parameter API Token: #{params[:api_token]}
         if ENV['TAB_PROVISIONING_PROFILE_PATH'] != nil
-          variable = `pwd`
-          puts `pwd`
-          puts variable
           provisioning_profile_path=ENV['TAB_PROVISIONING_PROFILE_PATH']
           provisioning_profile_uuid = `grep UUID -A1 -a #{provisioning_profile_path} | grep -io \"[-A-Z0-9]\\{36\\}\"`
           provisioning_profile_destination = "#{ENV['HOME']}/Library/MobileDevice/Provisioning\\\ Profiles/#{provisioning_profile_uuid.strip}.mobileprovision"
           `cp #{provisioning_profile_path} #{provisioning_profile_destination}`
           UI.success("Installed profile at path #{params[:provisioning_profile_path]} succesfully")
         else 
-          UI.error("TAB_PROVISIONING_PROFILE_PATH not provided")
-          UI.user_error! "Provide TAB_PROVISIONING_PROFILE_PATH"
+          UI.user_error! "TAB_PROVISIONING_PROFILE_PATH not defined"
         end
       end
 
