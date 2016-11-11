97afd7e3b83c504798278e5a6d5f174151ead372
Luciano Marisi <luciano@theappbusiness.com>
2016-11-11 16:11:22 +0000

    Removed unncessary functions

--- actions/install_provisioning_profile.rb
+++ actions/install_provisioning_profile.rb
@@ -24,39 +24,18 @@ module Fastlane
         "Installs a provisioning profile from a path"
       end
 
-      def self.details
-        # Optional:
-        # this is your chance to provide a more detailed description of this action
-        # "You can use this action to do cool things..."
-      end
-
       def self.available_options
-        # Define all options your action supports. 
-        
-        # Below a few examples
         [
           FastlaneCore::ConfigItem.new(key: :development,
                                        env_name: "TAB_PROVISIONING_PROFILE_PATH",
                                        description: "The path of the provisioning profile to install",
+                                       is_optional: false,
                                        is_string: false, # true: verifies the input is a string, false: every kind of value
                                        default_value: false) # the default value if the user didn't provide one
         ]
       end
 
-      def self.output
-        # Define the shared values you are going to provide
-        # Example
-        # [
-        #   ['INSTALL_PROVISIONING_PROFILE_CUSTOM_VALUE', 'A description of what this value contains']
-        # ]
-      end
-
-      def self.return_value
-        # If you method provides a return value, you can describe here what it does
-      end
-
       def self.authors
-        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
         ["Luciano Marisi @lucianomarisi"]
       end
 
